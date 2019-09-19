52066c0f36b055a760a43294c5013d04fead4753
yellow013 <wk9988@gmail.com>
2019-09-19 20:57:01 +0800

    update simple

--- simple-input/src/main/java/net/openhft/chronicle/queue/simple/input/PrintQueueMain.java
+++ simple-input/src/main/java/net/openhft/chronicle/queue/simple/input/PrintQueueMain.java
@@ -3,12 +3,13 @@ package net.openhft.chronicle.queue.simple.input;
 import net.openhft.chronicle.queue.DumpQueueMain;
 
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
 
 /**
  * Created by catherine on 18/07/2016.
  */
 public class PrintQueueMain {
-    public static void main(String[] args) throws FileNotFoundException {
-        DumpQueueMain.dump("queue");
-    }
+	public static void main(String[] args) throws FileNotFoundException {
+		DumpQueueMain.dump("backup-" + LocalDate.now());
+	}
 }
